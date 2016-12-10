fb5fc81eb39c96bf500cd86bd33b563b732faf89
a6885938a <332178065@qq.com>
2016-12-10 09:36:20 +0800

    1

--- www/wp-content/themes/tybj/index.php
+++ www/wp-content/themes/tybj/index.php
@@ -228,11 +228,17 @@ $post_list = new WP_Query(
 );
 
 ?>
+<<<<<<< Updated upstream
 
 
 <?php  $posts=query_posts($query_string .'&posts_per_page=6&ignore_sticky_posts=1'); ?>  
     <?php while (have_posts()) : the_post();?>
                    
+=======
+                 <?php  $posts=query_posts($query_string .'&posts_per_page=1'); ?>  
+    <?php while (have_posts()) : the_post();                    ?>
+                    <?php $category = get_the_category();//定义分类目录?>   
+>>>>>>> Stashed changes
                 <li class="col-xs-12 col-sm-6 col-md-4 op0" data-scroll-reveal="enter top over 1s ">
                     <a href="<?php the_permalink(); ?>">
                         <div class="goods-pic">
