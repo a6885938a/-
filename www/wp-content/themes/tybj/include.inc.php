ad186981e75059c743c4bed7c09f469e2453c733
a6885938a <332178065@qq.com>
2017-02-13 15:46:14 +0800

    1

--- www/wp-content/themes/tybj/include.inc.php
+++ www/wp-content/themes/tybj/include.inc.php
@@ -3,9 +3,11 @@ define('url_root','../'); //定义路径
 define('ty_of','/wp-content/themes/tybj/'); //定义路径  http://res.tybj-food.com/   本地 /wp-content/themes/tybj/
 define('ty_img','/wp-content/themes/tybj/'); //定义路径 http://image.tybj-food.com/  本地 /wp-content/themes/tybj/dist/images/
 define('version','?20161011'); //定义版本
+session_start();
+
 
 $A_webps='';
-if(isset($_COOKIE["webps"]))
+if(isset($_SESSION["webps"]))
 {
 $A_webps='_.webp';
 }else{
