1848ea5acf497b7210799272b07825dfd5bdd981
mark <332178065@qq.com>
2017-12-04 01:37:03 +0800

    add fade effect in about.vue

--- tybj-vue/pages/about.vue
+++ tybj-vue/pages/about.vue
@@ -1,50 +1,30 @@
 <template>
-
   <div>
-    <p>Hi from {{ name }}</p>
-    <nuxt-link to="/">Home page</nuxt-link>
+    <TitNav ref="nav" :title="''" />
+    <Swiper :goodsLists="bannerGoods" />
   </div>
 </template>
 <script>
+import TitNav from '~/components/TitNav.vue'
+import Swiper from '../components/about/Swiper.vue'
+import Aimg from '../src/images/mediaBg.jpg'
+
+
 export default {
-  layout: 'dark',
-  asyncData({ req }) {
+  data() {
     return {
-      name: req ? 'server' : 'client'
+      bannerGoods: [
+      { img: [Aimg] },
+           { img: ["http://img.tybj-food.com/2017/11/banner_02.jpg"] } 
+      ]
     }
+  },
+  components: {
+    TitNav,
+    Swiper,
+
+
   }
 }
 
 </script>
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-
-</style>
